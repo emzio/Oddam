e62374b4f78779c7a13c6b2ea5a1602fb9d19c7d
emzio <emziolkow@gmail.com>
2022-05-28 11:46:10 +0200

    wyświetlenie listy institutions dla admina

--- src/main/java/pl/coderslab/charity/controller/InstitutionController.java
+++ src/main/java/pl/coderslab/charity/controller/InstitutionController.java
@@ -1,6 +1,8 @@
 package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import pl.coderslab.charity.service.InstitutionService;
 
 @Controller
@@ -11,5 +13,9 @@ public class InstitutionController {
         this.institutionService = institutionService;
     }
 
-
+    @GetMapping("admin/institutions")
+    private String showAllDonations(Model model){
+        model.addAttribute("institutions", institutionService.findAll());
+        return "admin/institutions";
+    }
 }
