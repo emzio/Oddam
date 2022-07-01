47f2627765a1ecc84e99ab662f77532ccef51978
emzio <emziolkow@gmail.com>
2022-07-01 15:57:22 +0200

    dodane json dla USER: finById findAllEnabled

--- src/main/java/pl/coderslab/charity/restResurces/UserRestController.java
+++ src/main/java/pl/coderslab/charity/restResurces/UserRestController.java
@@ -1,15 +1,22 @@
 package pl.coderslab.charity.restResurces;
 
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import pl.coderslab.charity.entity.User;
 import pl.coderslab.charity.service.UserService;
 
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @AllArgsConstructor
@@ -17,21 +24,30 @@ public class UserRestController {
     private final UserService userService;
 
 
-    @ResponseBody
-    @GetMapping("/rest/users")
-    public String findAll(){
-        return userService.findAllEnabledUsers()
-                .stream().map(user -> user.toString())
-                .collect(Collectors.joining("<br>"));
+//    @ResponseBody
+//    @GetMapping("/rest")
+//    public String findAll(){
+//        return userService.findAllEnabledUsers()
+//                .stream().map(user -> user.toString())
+//                .collect(Collectors.joining("<br>"));
+//    }
+
+    @GetMapping("/rest")
+    List<User> findAll(){
+        return userService.findAllEnabledUsers();
     }
 
 
-    @ResponseBody
-    @GetMapping("/rest/user/{id}")
-    public  String findById(@PathVariable Long id){
-        return userService.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Rest entity not found")).toString();
+//    @ResponseBody
+    @GetMapping("/rest/{id}")
+//    public  String findById(@PathVariable Long id){
+//        return userService.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Rest entity not found")).toString();
+
+
+    EntityModel<User> findById(@PathVariable Long id){
+        User user = userService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Rest entity not found"));
+        return EntityModel.of(user,
+    linkTo(methodOn(UserRestController.class).findById(id)).withSelfRel(),
+    linkTo(methodOn(UserRestController.class).findAll()).withRel("rest"));
     }
-//    return EntityModel.of(employee, //
-//    linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-//    linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
 }
